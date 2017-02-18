0f6cf1cb96bf9c4a6a380a186a8a8523e7b48bc5
mk <matevzkren@gmail.com>
2017-02-18 22:54:22 +0100

    Implemented a decorator to assert general conditions, ie. that the data is suitable for visualization.

--- src/visualize.py
+++ src/visualize.py
@@ -141,8 +141,14 @@ def create_custom_html_document(figures, link_plots=False):
             '</html>']))
 
 
-def hyper_space_diagonal_counting():
-    pass
+def custom_assertions(visualization_method):
+    def inner(approximation_sets, names=None, *args):
+        assert (approximation_sets and len(approximation_sets) == len(names)
+                if names is not None else True)
+        assert len(set(a.shape[1] for a in approximation_sets)) == 1
+        return visualization_method(approximation_sets, names, *args)
+
+    return inner
 
 
 def distance_and_distribution_chart(approximation_sets, names=None,
@@ -275,103 +281,4 @@ def distance_and_distribution_chart(approximation_sets, names=None,
             distributions)
 
 if __name__ == "__main__":
-    # print all possible graph objects
-    print(graph_objs.graph_objs.__all__)
-
-    """ Scatter-plot matrix """
-    # load 3D approximation sets
-    lin_approx_set = np.loadtxt("../MOViE-mnozice/3d.linear.txt")
-    sph_approx_set = np.loadtxt("../MOViE-mnozice/3d.spherical.txt")
-
-    # create a pandas dataframe
-    df = pd.DataFrame(np.row_stack((lin_approx_set, sph_approx_set)),
-                      columns=['x', 'y', 'z'])
-
-    # denote the two data sets
-    df['Sets'] = pd.Series(['Linear'] * lin_approx_set.shape[0] +
-                           ['Spherical'] * sph_approx_set.shape[0])
-
-    # create plotly figure
-    fig = FF.create_scatterplotmatrix(df, index='Sets', diag='box',
-                                      size=10, height=1000, width=1000)
-
-    # plot
-    offline.plot(fig, show_link=False, filename="scatter_plot_matrix.html",
-                 auto_open=False)
-
-    """ 3d Scatter-plot """
-    knee_approx_set = np.loadtxt("../MOViE-mnozice/koleno3d.txt")
-    x, y, z = [knee_approx_set[:, i] for i in range(knee_approx_set.shape[1])]
-
-    marker1 = dict(line=dict(color='rgba(130, 130, 130, 0.14)', width=0.5),
-                   opacity=1, size=12)
-
-    trace1 = graph_objs.Scatter3d(x=x, y=y, z=z, mode='markers',
-                                  marker=marker1, hoverinfo='none')
-
-    data = [trace1]
-
-    layout = graph_objs.Layout(margin=dict(l=0, r=0, b=0, t=0),
-                               hovermode='closest', title='3d Scatter Plot')
-
-    fig = graph_objs.Figure(data=data, layout=layout)
-
-    offline.plot(fig, show_link=False, filename="3dscatter_with_knee.html",
-                 auto_open=False)
-
-    """ 3d Bubble Chart """
-    lin_approx_set_4d = np.loadtxt("../MOViE-mnozice/4d.linear.300.txt")
-    x, y, z, w = [lin_approx_set_4d[:, i] for i in range(lin_approx_set_4d.shape[1])]
-
-    marker1 = dict(sizemode='diameter', sizeref=0.03, size=w, color=w,
-                   colorscale='Viridis', colorbar=dict(title='w'),
-                   line=dict(color='rgb(140, 140, 170)'))
-
-    trace1 = graph_objs.Scatter3d(x=x, y=y, z=z, mode='markers', marker=marker1)
-
-    data = [trace1]
-
-    layout = dict(width=800, height=800, title='3d Bubble Chart',
-                  scene=dict(xaxis=dict(title='x', titlefont=dict(color='Orange')),
-                             yaxis=dict(title='y', titlefont=dict(color='rgb(220, 220, 220)')),
-                             zaxis=dict(title='z', titlefont=dict(color='rgb(220, 220, 220)')),
-                             bgcolor='rgb(20, 24, 54)'))
-
-    fig = dict(data=data, layout=layout)
-    offline.plot(fig, show_link=False, filename='3d_bubble_chart.html',
-                 auto_open=False)
-
-    """ Scatter plot with PCA dim. reduction """
-    lin_approx_set_4d = np.loadtxt("../MOViE-mnozice/4d.linear.300.txt")
-
-    pca = PCA(n_components=2)
-    x = lin_approx_set_4d
-    x = (x - np.mean(x, 0)) / np.std(x, 0)
-    x_r_1 = pca.fit(x).transform(x)
-
-    sph_approx_set_4d = np.loadtxt("../MOViE-mnozice/4d.spherical.300.txt")
-    x = sph_approx_set_4d
-    x = (x - np.mean(x, 0)) / np.std(x, 0)
-    x_r_2 = pca.fit(x).transform(x)
-
-    trace1 = graph_objs.Scatter(x=x_r_1[:, 0], y=x_r_1[:, 1], mode='markers')
-    trace2 = graph_objs.Scatter(x=x_r_2[:, 0], y=x_r_2[:, 1], mode='markers')
-
-    data = [trace1, trace2]
-    layout = graph_objs.Layout(autosize=False, width=900, height=700,
-                               hovermode='closest', title='PCA Scatter Plot',
-                               margin=dict(l=0, r=0, b=0, t=0))
-
-    fig = dict(data=data, layout=layout)
-
-    offline.plot(fig, show_link=False, filename='PCA_scatter_plot.html',
-                 auto_open=False)
-
-    """Distance and distribution chart"""
-    lin_approx_set = np.loadtxt("../MOViE-mnozice/4d.linear.300.txt")
-    sph_approx_set = np.loadtxt("../MOViE-mnozice/4d.spherical.300.txt")
-
-    result = distance_and_distribution_chart([lin_approx_set, sph_approx_set],
-                                             names=('linear', 'spherical'))
-
-    create_custom_html_document(result[0])
+    pass
